8df50ef9f06f2df3025c55b2b39b26e3961fc8bf
Pedro Esteves <up201705160@fe.up.pt>
2019-08-02 23:29:20 +0100

    Fixed rotation

--- gamejam+/Assets/Scripts/PlayerMovement.cs
+++ gamejam+/Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float movementSpeed;
+    public float movementSpeed, rotateSpeed;
     private Vector2 movementDirection;
     private float vertical, horizontal;
-
+    private float rotate;
 
     private void Start()
     {
-
+        if (movementSpeed == 0)
+            movementSpeed = 0.05f;
+        if (rotateSpeed == 0)
+            rotateSpeed = 2;
     }
 
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Transform body = this.transform.GetChild(0);
+        rotate = Input.GetAxis("Rotate");
+        if(rotate != 0)
+        {
+            body.RotateAround(body.transform.parent.position, Vector3.forward, rotate*rotateSpeed);
+
+            //body.Rotate(new Vector3(0, 0, 1), rotate * rotateSpeed);
+        }
 
         movementDirection = new Vector2(horizontal, vertical) * movementSpeed;
 
