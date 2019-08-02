8956409a2e4d7463990cea9d626413623714f055
Pedro Esteves <up201705160@fe.up.pt>
2019-08-02 21:51:39 +0100

    Added PLayer Movement

---
+++ gamejam+/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed;
+    private Vector2 movementDirection;
+    private float vertical, horizontal;
+
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        movementDirection = new Vector2(horizontal, vertical) * movementSpeed;
+
+        this.gameObject.transform.Translate(movementDirection);
+    }
+}
