59d290b1431eb049ae3bd3c09cc01acbdaebb620
GambuzX <mg_mesquita@hotmail.com>
2019-08-03 16:31:47 +0100

    Girlfriend UI message

--- gamejam+/Assets/Scripts/Girlfriend.cs
+++ gamejam+/Assets/Scripts/Girlfriend.cs
@@ -8,12 +8,16 @@ public class Girlfriend : MonoBehaviour
     public float reflectSpeed = 5f;
     public float stopDistance = 1f;
 
+    private bool movement_lock = false;
     private Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindObjectOfType<PlayerMovement>().transform;
+        InvokeRepeating("increaseSpeed", 2f, 2f);
+
+        movement_lock = false;
     }
 
     // Update is called once per frame
@@ -24,12 +28,26 @@ public class Girlfriend : MonoBehaviour
         {
             EndGame();
         }
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        if (!movement_lock)
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
 
     private void EndGame()
     {
-        Debug.Log("Game over");
+        movement_lock = true;
+        foreach(Transform obj in GameObject.Find("GirlfriendMsg").transform)
+        {
+            obj.gameObject.SetActive(true);
+        }
+        player.GetComponent<PlayerMovement>().enabled = false;
+        player.GetComponentInChildren<PolyShooter>().enabled = false;
+
+        //Invoke game over screen
+    }
+    
+    private void increaseSpeed()
+    {
+        speed += 0.1f;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
