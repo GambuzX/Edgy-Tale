ec8625bb5e785ec0ae3e46954d58c29f3f4940cf
GambuzX <mg_mesquita@hotmail.com>
2019-08-04 02:11:55 +0100

    minor fix

--- gamejam+/Assets/Scripts/PowerUpManager.cs
+++ gamejam+/Assets/Scripts/PowerUpManager.cs
@@ -9,18 +9,21 @@ public class PowerUpManager : MonoBehaviour
 
     private Image powerUpImage;
 
+    private Sprite background;
+
     // Start is called before the first frame update
     void Start()
     {
         activePowerUp = false;
         powerUpImage = GetComponent<Image>();
+        background = powerUpImage.sprite;
     }
 
     public void activatePowerUp(PowerUpType powerUp)
     {
         string name = PowerUp.PowerUpName(powerUp);
         powerUpImage.sprite = Resources.Load<GameObject>(name).GetComponent<SpriteRenderer>().sprite;
-        powerUpImage.color = new Color(255f, 255f, 255f, 255f);
+        //powerUpImage.color = new Color(255f, 255f, 255f, 255f);
 
         activePowerUp = true;
     }
@@ -28,8 +31,8 @@ public class PowerUpManager : MonoBehaviour
     public void disablePowerUp()
     {
         activePowerUp = false;
-        powerUpImage.sprite = null;
-        powerUpImage.color = new Color(108f, 108f, 108f, 168f);
+        powerUpImage.sprite = background;
+        //powerUpImage.color = new Color(108f, 108f, 108f, 168f);
     }
 
     public bool isPowerUpActive()
