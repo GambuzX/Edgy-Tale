c51be8f296df9837c1323a6724e8e3d404538391
Pedro Esteves <up201705160@fe.up.pt>
2019-08-04 09:51:48 +0100

    Fixed error

--- gamejam+/Assets/Scripts/SceneChanger.cs
+++ gamejam+/Assets/Scripts/SceneChanger.cs
@@ -14,7 +14,7 @@ public class SceneChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Skip"))
+        if (Input.anyKeyDown)
         {
             SceneManager.LoadScene("Menu");
         }
