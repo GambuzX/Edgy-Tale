a5ac85931b36d287bd01f5248bc0ad25b5a05bc1
Pedro Esteves <up201705160@fe.up.pt>
2019-08-04 02:38:32 +0100

    Fixed menu music

---
+++ gamejam+/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour
+{
+    private string actual_scene;
+
+    private void Awake()
+    {
+        actual_scene = SceneManager.GetActiveScene().name;
+        if (actual_scene != "Game" && actual_scene != "Transition")
+        {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+            if (objs.Length > 1)
+                Destroy(this.gameObject);
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        actual_scene = SceneManager.GetActiveScene().name;
+        if (actual_scene == "Game" || actual_scene == "Transition")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
