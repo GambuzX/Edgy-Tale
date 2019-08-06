552afea97e73ea567527254853d4e212bfc04a0a
Pedro Esteves <up201705160@fe.up.pt>
2019-08-06 23:03:50 +0100

    Added text to instructions and credits. Fixed problem with transition screen. Trying to fix the credits problem

---
+++ gamejam+/Assets/Scripts/MenuFade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuFade : MonoBehaviour
+{
+    private Text text;
+    private float initialTime;
+    private float i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        initialTime = Time.time;
+        i = 1;
+    }
+
+    private void Update()
+    {
+        bool fadeAway = false;
+        if ((int)(Time.time - initialTime) % 2 == 0)
+            fadeAway = true;
+
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            i -= Time.deltaTime / 2.0f;
+            text.color = new Color(1, 1, 1, i);
+        }
+        // fade from transparent to opaque
+        else
+        {
+            i += Time.deltaTime / 2.0f;
+            text.color = new Color(1, 1, 1, i);
+        }
+    }
+
+}
